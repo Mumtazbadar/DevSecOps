0f81bebe0a8d0c3a4a6480cd6d434486e16662bb
m.badar <m.badar@bayt.net>
2024-12-13 01:41:06 +0500

    create ec2 t3.medium -fixed protocol error for ssh v.4

--- Terraform/ec2-t3-medium.tf
+++ Terraform/ec2-t3-medium.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "ssh_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["111.88.221.177/32"]  # Replace with your actual IP address
+    cidr_blocks = ["111.88.221.177/32"]  
   }
 
   # Allow all outbound traffic
@@ -23,20 +23,20 @@ resource "aws_security_group" "ssh_sg" {
 
 # EC2 Key Pair
 resource "aws_key_pair" "key_pair" {
-  key_name   = "my-ssh-key"  # Replace with your desired key name
+  key_name   = "my-ssh-key" 
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDD/7uPJnZ6j4tN8HZuZhpnH4nJWBGOfWJCSV3Gp9/w8SqbyBEEACfiWIWEFDkfRgYGOnRhXNpLsk/Bf1Y+NmhcIhh0mG79H5bunbV+FHGoOE978bpSXsbuWBjiFgkGBNHsKWc8lAp+0hvHzsXNAtnS8KDaJN/FZifgFfnGajLnZNnMGSeGNRGQ0ownyrtFc5jqlkStABet4YNoj/Hmy9MecNwoTo3Tc2p4/jueSeINzubA7rZ39fpRtKEb5GC6UhN9F88Ti96OALecNLoyi+oZrOs4kyFptQwZlzKoFT2MPj2c2lsbUCIU8eC+PMIB+GB2TXD+l6+67m8AJQXE0Ybk7xRNG9bdEif3Zva2HKHDHUkuKazoIVSGPD5VieNGXBcUreNZ1NCO/wCM3QtgZfWP2BR2ibx5K+zyeh08IbXQXPJ90+MA9NbMLRuqka6CJFxDIcfIA3Gmnuq9CIAZVPcEScopmapMgBzVF0VNxrRaq1kpQ1JGOK235lwCYde/yGxDMe97LiRBc4Ao69DsofGU+pFVewVHzm6IT2q0fGK97Gsx8M7SmIPK6la9ATMTzEKdJ0afdA3Vr62LiUbEsy8tO0GDX1DaxJuUqEnxaCkeIIz1jr6GyBztsMOveFUnTConJUC1zZWd502ut2+vp56rX+0c+l0CPSYjDuPsblZAQw== mbadar-ec2t3.medium"
 }
 
 # Launch EC2 Instance (t3.medium)
 resource "aws_instance" "example" {
-  ami           = "ami-0a91cd140a1fc148a"  # Amazon Linux 2 AMI ID for us-west-2, you can check the latest AMI in AWS
+  ami           = "ami-0a91cd140a1fc148a"
   instance_type = "t3.medium"
   key_name      = aws_key_pair.key_pair.key_name
   security_groups = [aws_security_group.ssh_sg.name]
   
   # Instance metadata
   tags = {
-    Name = "MyEC2Instance"
+    Name = "MyEC2InstancefromTF"
   }
 
   # Enable monitoring (optional, but useful for production instances)
